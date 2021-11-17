fec57860f876ebaa50119f416cd92527035ba634
Yu Feng <fengyu2alan@gmail.com>
2021-11-17 09:17:49 +0800

    Add files via upload

---
+++ theme4paper.R
@@ -0,0 +1,29 @@
+theme_boxborder <- function(base_size = 9, 
+                            base_family = "Arial",
+                            base_line_size = 0.18, 
+                            base_rect_size = 0.18) {
+  theme_bw(base_size = base_size,
+           base_family = base_family,
+           base_line_size = base_line_size,
+           base_rect_size = base_rect_size) %+replace%
+    theme(
+      panel.grid = element_blank(),
+      plot.title = element_text(hjust = 0.5),
+      legend.background = element_blank()
+      )
+}
+
+
+theme_lborder <- function(base_size = 9, 
+                      base_family = "Arial",
+                      base_line_size = 0.18, 
+                      base_rect_size = 0.18) {
+  theme_classic(base_size = base_size,
+                base_family = base_family,
+                base_line_size = base_line_size,
+                base_rect_size = base_rect_size) %+replace%
+    theme(
+      plot.title = element_text(hjust = 0.5),
+      legend.background = element_blank()
+    )
+}
