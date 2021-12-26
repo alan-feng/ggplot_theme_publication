8d70ca4be1a618a9dba09141bcd01d5a23e62e57
Yu Feng <fengyu2alan@gmail.com>
2021-12-26 15:17:17 +0800

    Update theme4paper.R

--- theme4paper.R
+++ theme4paper.R
@@ -20,19 +20,20 @@ theme_boxborder <- function(base_size = 9,
            base_line_size = base_line_size,
            base_rect_size = base_rect_size) %+replace%
     theme(
-      panel.grid = element_blank(),
-      panel.border = element_rect(fill = NA, colour = "black", size = rel(1)),
-      plot.title = element_text(hjust = 0.5),
-      legend.background = element_blank(),
       axis.text = element_text(size = rel(0.9), colour = "black"),
       axis.ticks = element_line(colour = "black", size = rel(1)),
+      legend.background = element_blank(),
       legend.key.size = unit(0.3, "lines"),
       legend.text = element_text(size = rel(0.7)),
       legend.title = element_text(hjust = 0.2, vjust = 0.8, size = rel(0.6)),
       legend.margin = margin(0.1, 0.1, 0.1, 0.1, unit = "lines"),
+      panel.grid = element_blank(),
+      panel.border = element_rect(fill = NA, colour = "black", size = rel(1)),
       strip.background = element_rect(fill = NA, colour = NA),
       strip.text = element_text(colour = "black", size = rel(1),
-                                margin = margin(0.1, 0.1, 0.1, 0.1, unit = "lines"))
+                                margin = margin(0.1, 0.1, 0.1, 0.1, unit = "lines")),
+      plot.title = element_text(hjust = 0.5),
+      plot.margin = margin(base_size/2, base_size/2 + 2, base_size/2, base_size/2)
     )
 }
 
@@ -48,17 +49,18 @@ theme_lborder <- function(base_size = 9,
                 base_line_size = base_line_size,
                 base_rect_size = base_rect_size) %+replace%
     theme(
-      plot.title = element_text(hjust = 0.5),
-      legend.background = element_blank(),
       axis.text = element_text(size = rel(0.9), colour = "black"),
       axis.ticks = element_line(colour = "black", size = rel(1)),
       axis.line = element_line(colour = "black", size = rel(1)),
+      legend.background = element_blank(),
       legend.key.size = unit(0.3, "lines"),
       legend.text = element_text(size = rel(0.7)),
       legend.title = element_text(hjust = 0.2, vjust = 0.8, size = rel(0.6)),
       legend.margin = margin(0.1, 0.1, 0.1, 0.1, unit = "lines"),
       strip.background = element_rect(fill = NA, colour = NA),
       strip.text = element_text(colour = "black", size = rel(1),
-                                margin = margin(0.1, 0.1, 0.1, 0.1, unit = "lines"))
+                                margin = margin(0.1, 0.1, 0.1, 0.1, unit = "lines")),
+      plot.title = element_text(hjust = 0.5),
+      plot.margin = margin(base_size/2, base_size/2 + 2, base_size/2, base_size/2)
     )
 }
