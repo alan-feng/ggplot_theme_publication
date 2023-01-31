55712b5d718c367ef30ca05e714576e6e33af331
Yu Feng <fengyu2alan@gmail.com>
2023-01-31 15:09:19 +0100

    Update ggtheme_publication.R

--- ggtheme_publication.R
+++ ggtheme_publication.R
@@ -1,3 +1,5 @@
+library(ggplot2)
+
 # Set default line width to 0.18mm for the geom objects and text size to 9 pt
 update_geom_defaults("line", list(linewidth = 0.18/0.75))
 update_geom_defaults("path", list(linewidth = 0.18/0.75))
